460e2a85c1876b326adaa54c228817c72c47cfea
AER-S <89151307+AER-S@users.noreply.github.com>
2022-03-09 22:11:37 +0100

    Creating the Chests

---
+++ Assets/Scripts/Chests/Chest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Chests
+{
+    [UnityEngine.CreateAssetMenu(fileName = "NewChestType", menuName = "User/Chests/Chest Type", order = 0)]
+    public class Chest : UnityEngine.ScriptableObject
+    {
+        [System.Serializable]
+        private struct CurrencyRange
+        {
+            public int min;
+            public int max;
+        }
+
+        [SerializeField] private float timeToOpen;
+        [SerializeField] private CurrencyRange coinsRange;
+        [SerializeField] private CurrencyRange gemsRange;
+    }
+}
