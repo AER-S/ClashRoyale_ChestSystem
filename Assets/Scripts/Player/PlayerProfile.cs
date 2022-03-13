ef7d91d1678ae7a6add83deb8947f6099de7f807
AER-S <89151307+AER-S@users.noreply.github.com>
2022-03-13 13:30:38 +0100

    Adding the ChestSlots to the player profile

--- Assets/Scripts/Player/PlayerProfile.cs
+++ Assets/Scripts/Player/PlayerProfile.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Chests;
 using UnityEngine;
 
 namespace Player
@@ -8,6 +10,14 @@ namespace Player
         [SerializeField] private string name = "Put a Name here.";
         [SerializeField] private int coins = 1000;
         [SerializeField] private int gems = 20;
+
+        [SerializeReference] private ChestSlot slot0;
+        [SerializeReference] private ChestSlot slot1;
+        [SerializeReference] private ChestSlot slot2;
+        [SerializeReference] private ChestSlot slot3;
+
+        private ChestSlot _currentlyUnlocking;
+        private Queue<ChestSlot> _toUnlockSlots;
 
         public string Name => name;
         public int Coins => coins;
